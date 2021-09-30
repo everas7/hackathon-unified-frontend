d70b107db9668a184e8536b86526477faa1df692
William Bang <will.h.bang@gmail.com>
2021-09-30 15:47:30 -0400

    Add data directory

---
+++ data/conversations.js
@@ -0,0 +1,79 @@
+const conversations = [
+    {
+        contact: {
+            conversationRead: true,
+            contactId: 12345,
+            firstName: 'John M',
+            lastName: 'Bradley',
+            primaryEmailAddress: 'john.m.bradley@gmail.com',
+            primaryPhoneNumber: '2125551234',
+        },
+        message: {
+            documentType: 'Agentz',
+            messageBody: 'We value your opinion!',
+            timestamp: '2021-09-29T19:09:12.359Z',
+        },
+    },
+    {
+        contact: {
+            conversationRead: true,
+            contactId: 12346,
+            firstName: null,
+            lastName: null,
+            primaryEmailAddress: null,
+            primaryPhoneNumber: '2125551235',
+        },
+        message: {
+            documentType: 'Sms',
+            messageBody: 'Lorem ipsum dolor',
+            timestamp: '2021-09-28T19:09:12.359Z',
+        },
+    },
+    {
+        contact: {
+            conversationRead: true,
+            contactId: 12347,
+            firstName: 'Tyrrell',
+            lastName: 'Andrijana',
+            primaryEmailAddress: 't.Andrijana@gmail.com',
+            primaryPhoneNumber: '2125551236',
+        },
+        message: {
+            documentType: 'Facebook',
+            messageBody: 'sit amet, consectetur',
+            timestamp: '2021-09-27T19:09:12.359Z',
+        },
+    },
+    {
+        contact: {
+            conversationRead: true,
+            contactId: 12347,
+            firstName: 'Ivo',
+            lastName: 'Haris',
+            primaryEmailAddress: 't.Andrijana@gmail.com',
+            primaryPhoneNumber: '2125551236',
+        },
+        message: {
+            documentType: 'PhoneCall',
+            messageBody: 'adipiscing elit, sed do eiusmod',
+            timestamp: '2021-09-26T19:09:12.359Z',
+        },
+    },
+    {
+        contact: {
+            conversationRead: true,
+            contactId: 12347,
+            firstName: null,
+            lastName: null,
+            primaryEmailAddress: 'k.tychon@gmail.com',
+            primaryPhoneNumber: null,
+        },
+        message: {
+            documentType: 'Agentz',
+            messageBody: 'tempor incididunt ut labore et dolore magna aliqua.',
+            timestamp: '2021-09-25T19:09:12.359Z',
+        },
+    },
+];
+
+export default conversations;
