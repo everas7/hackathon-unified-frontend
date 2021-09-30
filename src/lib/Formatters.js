9161b1f791106662b603d58461cea5eaa8da83f7
William Bang <will.h.bang@gmail.com>
2021-09-30 14:06:46 -0400

    campaign stats component

--- src/lib/Formatters.js
+++ src/lib/Formatters.js
@@ -68,6 +68,25 @@ export function formatName({
 }
 
 /**
+ * Returns numbers formatted as strings with commas like: "45,70"
+ *
+ * @param {Object} params
+ * @param {number} params.number - Number to be formatted
+ * @returns {string} Formatted number
+ */
+export function formatNumber({ number }) {
+    if (number === undefined || number === null) {
+        return '';
+    }
+
+    const parts = number.toString().split('.');
+
+    parts[0] = parts[0].replace(/\B(?=(\d{3})+(?!\d))/g, ',');
+
+    return parts.join('.');
+}
+
+/**
  * Returns the phone number formatted like: "(737) 200-1234"
  *
  * @param {Object} params
